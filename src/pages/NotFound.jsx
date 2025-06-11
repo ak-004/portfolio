bdf4c4d94c818625cdbe9150432d43f2e0d5e37c
ak-004 <126799414+ak-004@users.noreply.github.com>
2025-06-11 15:02:33 +0530

    Add files via upload

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className='bg-red-500'>
+            <p>BYE</p>
+        </div>
+    )
+}
+
+export default NotFound
